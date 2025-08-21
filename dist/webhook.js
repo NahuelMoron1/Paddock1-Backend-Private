0ce5c7cdaceedbc47e51a6acdbd66d9f8ca07902
moronnahuu1 <nahuelarielmoron1@gmail.com>
2025-08-21 12:03:49 -0300

    remove testing message

--- dist/webhook.js
+++ dist/webhook.js
@@ -9,7 +9,6 @@ const router = express_1.default.Router();
 router.post("/github-webhook", (req, res) => {
     // Opcional: validar el secret si lo pusiste en GitHub
     console.log("Webhook recibido de GitHub");
-    console.log("testing");
     (0, child_process_1.exec)("cd /home/safe365api/htdocs/api.safe-365.online/Safe365-Backend && git pull origin main && npm install --production && pm2 restart mi-Backend", (err, stdout, stderr) => {
         if (err) {
             console.error(err);
