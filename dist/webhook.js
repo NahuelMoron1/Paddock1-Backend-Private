f7a06f22579f622c8feb4cae57036046c4e47996
moronnahuu1 <nahuelarielmoron1@gmail.com>
2025-08-21 12:01:19 -0300

    testing

--- dist/webhook.js
+++ dist/webhook.js
@@ -1,26 +1,22 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const child_process_1 = require("child_process");
 const router = express_1.default.Router();
 router.post("/github-webhook", (req, res) => {
-  // Opcional: validar el secret si lo pusiste en GitHub
-  console.log("Webhook recibido de GitHub");
-  (0, child_process_1.exec)(
-    "cd /home/safe365api/htdocs/api.safe-365.online/Safe365-Backend && git pull origin main && npm install --production && pm2 restart mi-backend",
-    (err, stdout, stderr) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send("Error ejecutando deploy");
-      }
-      console.log(stdout);
-      res.send("Deploy realizado correctamente");
-    }
-  );
+    // Opcional: validar el secret si lo pusiste en GitHub
+    console.log("Webhook recibido de GitHub");
+    console.log("testing");
+    (0, child_process_1.exec)("cd /home/safe365api/htdocs/api.safe-365.online/Safe365-Backend && git pull origin main && npm install --production && pm2 restart mi-Backend", (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Error ejecutando deploy");
+        }
+        console.log(stdout);
+        res.send("Deploy realizado correctamente");
+    });
 });
 exports.default = router;
