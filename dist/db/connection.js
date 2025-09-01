e1dd09857da2fea1ef308bbe3f0cce962edec693
moronnahuu1 <nahuelarielmoron1@gmail.com>
2025-09-01 11:49:50 -0300

    timezone

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -2,15 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const config_1 = require("../models/config");
-const config_2 = require("../models/config");
-const config_3 = require("../models/config");
-const config_4 = require("../models/config");
-const config_5 = require("../models/config");
-const sequelize = new sequelize_1.Sequelize(config_2.DB_NAME, config_5.DB_USER, config_3.DB_PASSWORD, {
+const sequelize = new sequelize_1.Sequelize(config_1.DB_NAME, config_1.DB_USER, config_1.DB_PASSWORD, {
     host: config_1.DB_HOST,
-    port: config_4.DB_PORT,
+    port: config_1.DB_PORT,
     dialect: "mysql",
-    timezone: "-03:00",
+    timezone: "America/Argentina/Buenos_Aires",
     dialectOptions: {
         timezone: "Etc/GMT+3", // para el lado del servidor MySQL
         dateStrings: true,
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,14 +1,16 @@
 import { Sequelize } from "sequelize";
-import { DB_HOST } from "../models/config";
-import { DB_NAME } from "../models/config";
-import { DB_PASSWORD } from "../models/config";
-import { DB_PORT } from "../models/config";
-import { DB_USER } from "../models/config";
+import {
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+} from "../models/config";
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   port: DB_PORT,
   dialect: "mysql",
-  timezone: "-03:00",
+  timezone: "America/Argentina/Buenos_Aires",
   dialectOptions: {
     timezone: "Etc/GMT+3", // para el lado del servidor MySQL
     dateStrings: true,
