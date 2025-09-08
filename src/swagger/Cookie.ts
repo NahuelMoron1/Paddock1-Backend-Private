1d350f8cf78ea2f270554b549c62208ce72f1496
moronnahuu1 <nahuelarielmoron1@gmail.com>
2025-09-08 13:12:01 -0300

    Cookies added to swagger

---
+++ src/swagger/Cookie.ts
@@ -0,0 +1,37 @@
+/** CHECK IF COOKIE EXISTS
+ * @swagger
+ * /api/cookie/check/{cookieName}:
+ *   get:
+ *     summary: Verificar si existe una cookie
+ *     tags: [Cookies]
+ *     parameters:
+ *       - in: path
+ *         name: cookieName
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: nombre de la cookie
+ *     responses:
+ *       200:
+ *         description: "true | false"
+ */
+
+/** GET TOKEN
+ * @swagger
+ * /api/cookie/get/{cookieName}:
+ *   get:
+ *     summary: Obtener las reviews de un medico
+ *     tags: [Cookies]
+ *     parameters:
+ *       - in: path
+ *         name: cookieName
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: nombre de la cookie
+ *     responses:
+ *       200:
+ *         description: "Información de la cookie"
+ *       401:
+ *         description: "Acceso denegado"
+ */
